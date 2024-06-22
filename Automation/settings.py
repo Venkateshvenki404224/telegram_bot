083dc667f06954148859b9412460140b4a7796fb
M Venkatesh <venkateshvenki404224@gmail.com>
2024-06-22 05:14:15 +0000

    Dockerized the django project

--- Automation/settings.py
+++ Automation/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import os
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,24 +81,23 @@ WSGI_APPLICATION = 'Automation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
+
+
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "admin_django",
+#         "USER": "admin",
+#         "PASSWORD": "password",
+#         "HOST": "postgresql.selfmade.ninja",
+#         "PORT": "5432",
 #     }
 # }
-
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "admin_django",
-        "USER": "admin",
-        "PASSWORD": "password",
-        "HOST": "postgresql.selfmade.ninja",
-        "PORT": "5432",
-    }
-}
 
 
 
